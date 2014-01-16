9a37184b08e1116022870f78faccf2aa85d7b715
Tiago Garcia <tiagoromerogarcia@gmail.com>
2014-01-16 14:13:35 -0200

    finishing all users summary

--- app/scripts/models/summary.js
+++ app/scripts/models/summary.js
@@ -4,11 +4,70 @@ define([
   'underscore',
   'backbone',
   'common',
-  'questions-data'
-], function (_, Backbone, Common, QuestionsData) {
+  'questions-data',
+  'models/user'
+], function (_, Backbone, Common, QuestionsData, UserModel) {
   'use strict';
 
   var SummaryModel = Backbone.Model.extend({
+    answersPercentage: {},
+
+    initialize: function() {
+      var answersStats;
+
+      answersStats = this.buildAnswersStats();
+
+      this.buildAnswersPercentage(answersStats);
+    },
+
+    buildAnswersStats: function() {
+      var answersStats, usersSequence, userId, userModel;
+
+      answersStats = {};
+      usersSequence = localStorage.getItem(Common.Repositories.usersSequence);
+
+      for (userId = 1; userId <= usersSequence; userId++) {
+        userModel = new UserModel({ id: userId });
+        userModel.fetch();
+
+        _.each(userModel.get('questions'), function(question) {
+          if (!answersStats[question.id]) {
+            answersStats[question.id] = [];
+          }
+
+          _.each(question.answers, function(answer) {
+            answersStats[question.id][answer] = answersStats[question.id][answer] + 1 || 1;
+          });
+        });
+      }
+
+      return answersStats;
+    },
+
+    buildAnswersPercentage: function(answersStats) {
+      var self, answerTotals;
+
+      self = this;
+      answerTotals = [];
+
+      _.each(answersStats, function(answers, questionId) {
+        answerTotals[questionId] = _.reduce(answers, function(total, num){
+          return total + num;
+        }, 0);
+      });
+
+      _.each(answersStats, function(answers, questionId) {
+        if (!self.answersPercentage[questionId]) {
+          self.answersPercentage[questionId] = Array.apply(null, new Array(5)).map(Number.prototype.valueOf,0);
+        }
+
+        _.each(answers, function(answer, answerId) {
+          self.answersPercentage[questionId][answerId] =
+            parseFloat(answer/answerTotals[questionId] * 100).toFixed(2);
+        });
+      });
+    },
+
     parse: function(response){
       response.score = this.calculateScore(response.questions);
       response.result = this.calculateResult(response.score);
@@ -26,7 +85,7 @@ define([
         }
       });
 
-      return (correctQuestions / QuestionsData.length) * 100;
+      return correctQuestions / QuestionsData.length * 100;
     },
 
     calculateResult: function(score) {
@@ -46,7 +105,10 @@ define([
     },
 
     mergeQuestions: function(answeredQuestions) {
-      var questions = [];
+      var questions, self;
+
+      self = this;
+      questions = [];
 
       _.each(answeredQuestions, function(answeredQuestion) {
         var question, originalQuestion, userAnswers;
@@ -74,7 +136,8 @@ define([
 
           question.answers.push({
             answer: originalAnswer,
-            result: result
+            result: result,
+            percentage: self.answersPercentage[question.id][index]
           });
         });
 
